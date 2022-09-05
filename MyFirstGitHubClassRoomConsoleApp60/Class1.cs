c5486af274d13075e0992788499676170853260b
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2022-09-05 12:43:30 +0200

    se jeg kan kode moder

---
+++ MyFirstGitHubClassRoomConsoleApp60/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Class1
+    {
+        private int _tal;
+
+        private int _dag;
+
+        private string _navn;
+
+        private string _title;
+
+        private string _description;
+
+        public int Tal
+        {
+            get { return _tal; }
+            set { _tal = value; }
+        }
+
+        public int Dag
+        {
+            get { return _dag; }
+            set { _dag = value; }
+        }
+
+        public string Navn
+        {
+            get { return _navn; }
+            set
+            {
+                _navn = value;
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                _description = value;
+            }
+        }
+
+    }
+}
